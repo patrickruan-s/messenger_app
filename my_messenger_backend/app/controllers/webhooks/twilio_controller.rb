d7dfd8872cc524f536eb5557d849e646f89806c4
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2026-02-28 10:38:28 -0600

    styling for message bubbles

--- my_messenger_backend/app/controllers/webhooks/twilio_controller.rb
+++ my_messenger_backend/app/controllers/webhooks/twilio_controller.rb
@@ -1,5 +1,5 @@
 class Webhooks::TwilioController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, raise: false
 
   def sms
     Message.create!(
